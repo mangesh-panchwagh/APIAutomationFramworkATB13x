e7efd21d0437f00c61967de9f73e8235b4f13c63
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2025-09-21 11:26:56 +0530

    commit

--- src/main/java/endpoints/APIConstants.java
+++ src/main/java/endpoints/APIConstants.java
@@ -11,4 +11,10 @@ public class APIConstants {
 	// Project #2 - App vwo Login
 	public static String APP_VWO_URL = "https://app.vwo.com/login";
 	// public static String BASE_URL = // Fetch this from Excel file
+	
+	// Project #3
+	public static String BASE_URL_PROJECT_3 = "";
+	public static String UPDATE_STATUS = "";
+	
+	
 }
